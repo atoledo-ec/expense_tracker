da0a758b92d5acce0945791cecc6ecf0fdc16790
Alejandro Toledo <atoledofr@gmail.com>
2025-11-21 21:23:10 -0500

    add url setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This MUST match your repository name exactly (including the typo 'expese')
+  base: "/expese_tracker/",
 })
